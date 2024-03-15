1aa7ef4234de87477de235af06e9ddc4b4d847c0
liuwei1206 <yanqing_0301>
2024-03-15 08:02:34 +0100

    update

--- eacl_miniconf/rocketchat/cli.py
+++ eacl_miniconf/rocketchat/cli.py
@@ -26,6 +26,10 @@ def paper_id_to_channel_name(paper_id: str):
     channel_name = channel_name.replace(".", "-")
     return channel_name
 
+def write_into_json(datas, file_name):
+    with open(file_name, "w", encoding="utf-8") as f:
+        for data in datas:
+            f.write("%s\n"%(json.dumps(data, ensure_ascii=False)))
 
 API_path = "/api/v1/"
 CUSTOM_EMOJI_DIR = Path("rocketchat-custom-emojis/")
@@ -88,12 +92,21 @@ class EaclRcHelper:
             self.rocket.channels_set_topic(channel_id, topic).json()
             self.rocket.channels_set_description(channel_id, description).json()
 
+    def delete_channel(
+        self, room_id=None, channel=None
+    ):
+        """
+        delete a channel using either room_id or channel_name
+        """
+        self.rocket.channels_delete(room_id=room_id, channel=channel)
+
     def create_tutorial_channels(self):
         existing_channels = set(self.get_channel_names())
         skipped = 0
         created = 0
+        datas = []
         for tutorial in track(self.booklet["tutorials"]):
-            tutorial_id = tutorial["id"].replace("t", "")
+            tutorial_id = tutorial["id"].replace("T", "")
             channel_name = f"tutorial-{tutorial_id}"
             author_string = ", ".join(tutorial["hosts"])
             title = tutorial["title"]
@@ -102,15 +115,23 @@ class EaclRcHelper:
             self.create_channel(channel_name, topic, tutorial["desc"], create=create)
             created += 1
 
+            sample = {}
+            sample['tutorial_id'] = tutorial_id
+            sample['tutorial_channel'] = channel_name
+            sample['tutorial_title'] = title
+            datas.append(sample)
+
         print(
             f"Total tutorials: {len(self.conference.papers)}, Created: {created} Skipped: {skipped} Total: {created + skipped}"
         )
+
+        # write_into_json(datas, "tuturial_channels.json")
 
     def create_workshop_channels(self):
         existing_channels = set(self.get_channel_names())
         skipped = 0
         created = 0
-
+        datas = []
         for ws in track(self.workshops):
             if ws["short_name"] == "inputs":
                 workshop_id = ws["anthology_venue_id"]
@@ -123,17 +144,31 @@ class EaclRcHelper:
             self.create_channel(channel_name, topic, topic, create=create)
             created += 1
 
+            sample = {}
+            sample['workshop_id'] = workshop_id
+            sample['workshop_channel'] = channel_name
+            sample['workshop_title'] = channel_name
+            datas.append(sample)
+
         print(
             f"Total workshops: {len(self.conference.papers)}, Created: {created} Skipped: {skipped} Total: {created + skipped}"
         )
+
+        # write_into_json(datas, "workshop_channels.json")
 
     def create_paper_channels(self):
         existing_channels = set(self.get_channel_names())
         skipped = 0
         created = 0
+        datas = []
         for paper in track(self.conference.papers.values()):
             if paper.is_paper:
                 channel_name = paper_id_to_channel_name(paper.id)
+                sample = {}
+                sample['paper_id'] = paper.id
+                sample['channel_name'] = channel_name
+                sample['paper_title'] = paper.title
+                datas.append(sample)
                 if channel_name in existing_channels:
                     skipped += 1
                 else:
@@ -141,7 +176,7 @@ class EaclRcHelper:
                     topic = f"{paper.title} - {author_string}"
                     self.create_channel(channel_name, topic, paper.abstract)
                     created += 1
-
+        # write_into_json(datas, "paper_channels.json")
         print(
             f"Total papers: {len(self.conference.papers)}, Created: {created} Skipped: {skipped} Total: {created + skipped}"
         )
@@ -251,7 +286,34 @@ def hydra_main(cfg: DictConfig):
             )
             channel_names = helper.get_channel_names()
             print(channel_names)
+    elif command == "delete_channels":
+        with sessions.Session() as session:
+            helper = EaclRcHelper(
+                user_id=cfg.user_id,
+                auth_token=cfg.auth_token,
+                server=cfg.server,
+                session=session,
+                program_json_path=Path(cfg.program_json_path),
+                booklet_json_path=Path(cfg.booklet_json_path),
+                workshops_yaml_path=Path(cfg.workshops_yaml_path),
+                dry_run=cfg.dry_run,
+            )
+            channel_names = helper.get_channel_names()
 
+            ## 1. delete paper channel
+            # for channel in channel_names:
+            #     if "paper-" in channel.lower():
+            #         helper.delete_channel(channel=channel)
+
+            ## 2. delete tutorial channel
+            for channel in channel_names:
+                if "tutorial-" in channel.lower():
+                    helper.delete_channel(channel=channel)
+
+            ## 3. delete workshop channel
+            # for channel in channel_names:
+            #     if "workshop-" in channel.lower():
+            #         helper.delete_channel(channel=channel)
 
 if __name__ == "__main__":
     hydra_main()
